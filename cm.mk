fe49ee0d8a43378f4a19f2ad48c0f2f1272b4586
PhantomGamers <phantom@sundergaming.com>
2012-09-15 15:48:29 -0400

    Update d2vzw fingerprint to latest release

--- cm.mk
+++ cm.mk
@@ -9,7 +9,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=d2vzw TARGET_DEVICE=d2vzw BUILD_FINGERPRINT="d2vzw-user 4.0.4 IMM76D I535VRALF2 release-keys" PRIVATE_BUILD_DESC="Verizon/d2vzw/d2vzw:4.0.4/IMM76D/I535VRALF2:user/release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=d2vzw TARGET_DEVICE=d2vzw BUILD_FINGERPRINT="d2vzw-user 4.0.4 IMM76D I535VRALG7 release-keys" PRIVATE_BUILD_DESC="Verizon/d2vzw/d2vzw:4.0.4/IMM76D/I535VRALG7:user/release-keys"
 
 PRODUCT_NAME := cm_d2vzw
 PRODUCT_DEVICE := d2vzw
