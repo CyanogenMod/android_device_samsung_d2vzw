0aa5e739b26d77fc82905c24133034de57ef40fb
invisiblek <dan.pasanen@gmail.com>
2012-12-22 11:39:18 -0600

    d2vzw: no need to inherit props twice
    Change-Id: Ic2a9793b5501b97ea600ebf6ec6ed9df36da9b2e

--- device.mk
+++ device.mk
@@ -24,5 +24,3 @@ DEVICE_PACKAGE_OVERLAYS += device/samsung/d2-common/overlay-cdma
 
 # Inherit from d2-common
 $(call inherit-product, device/samsung/d2-common/d2-common.mk)
-
-$(call inherit-product-if-exists, vendor/samsung/d2vzw/d2vzw-vendor.mk)
