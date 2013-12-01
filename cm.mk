f5bc00c03d9adc684daeb82158cb9c37eaaa4862
Matt Mower <mowerm@gmail.com>
2013-12-01 15:42:17 -0600

    d2vzw: access vzw apps in play store
    Change-Id: I8f6bebcf80fafeaa94bd72e7302c240eb8beb6e1

--- cm.mk
+++ cm.mk
@@ -6,7 +6,13 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=d2vzw TARGET_DEVICE=d2vzw BUILD_FINGERPRINT="Verizon/d2vzw/d2vzw:4.1.2/JZO54K/I535VRBMF1:user/release-keys" PRIVATE_BUILD_DESC="d2vzw-user 4.1.2 JZO54K I535VRBMF1 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=d2vzw \
+    TARGET_DEVICE=d2vzw \
+    BUILD_FINGERPRINT="Verizon/d2vzw/d2vzw:4.1.2/JZO54K/I535VRBMF1:user/release-keys" \
+    PRIVATE_BUILD_DESC="d2vzw-user 4.1.2 JZO54K I535VRBMF1 release-keys"
+
+PRODUCT_GMS_CLIENTID_BASE := android-verizon
 
 PRODUCT_NAME := cm_d2vzw
 PRODUCT_DEVICE := d2vzw
