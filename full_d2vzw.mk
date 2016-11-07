dbc41fa2645f3827e23741a75c2d9fd872335860
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-11-07 11:10:20 -0600

    d2vzw: this isn't maguro...
    Change-Id: I8b42f04c7ec96646da44ca2cc2042de4678abdc9

--- full_d2vzw.mk
+++ full_d2vzw.mk
@@ -11,16 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
- 
-#
-# This file is the build configuration for a full Android
-# build for maguro hardware. This cleanly combines a set of
-# device-specific aspects (drivers) with a device-agnostic
-# product configuration (apps). Except for a few implementation
-# details, it only fundamentally contains two inherit-product
-# lines, full and maguro, hence its name.
-#
- 
+
 PRODUCT_PACKAGES := \
     Stk
 
